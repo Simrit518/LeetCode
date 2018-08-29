4032ac4eb8ab5942794eb1d6cb97acfb9c2b301b
Simrit <lz1995@126.com>
2018-08-29 16:25:13 +0800

    2018-8-29

--- src/ArrayEX.java
+++ src/ArrayEX.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ArrayEX {
@@ -6,19 +7,23 @@ public class ArrayEX {
         Scanner in =new Scanner(System.in);
         String[] s;
         s=in.nextLine().split(",");
-        int[] digits=new int[s.length];
+        int[] nums=new int[s.length];
         for (int i=0;i<s.length;i++){
-            digits[i]=Integer.parseInt(s[i]);
+            nums[i]=Integer.parseInt(s[i]);
+        }
+        int target=in.nextInt();
+        ArrayEX arrayEX=new ArrayEX();
+        int len=arrayEX.removeElement(nums,target);
+        System.out.println(len);
+
+        for (int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
         }
 
-        System.out.println(Arrays.toString(plusOne(digits)));
-//        for (int i=0;i<digits.length;i++){
-//            System.out.println(digits[i]);
-//        }
 
     }
 
-    public static int searchinsertpos(int[] nums,int target){
+    public  int searchinsertpos(int[] nums,int target){
         if (nums==null|| nums.length<=0)
             return -1;
         int index=0;
@@ -42,7 +47,7 @@ public class ArrayEX {
 //        return index;
     }
     //太巧了这方法，一旦执行if就返回！！！
-    public static int[] plusOne(int[] digits) {
+    public  int[] plusOne(int[] digits) {
 
         int n = digits.length;
         for(int i=n-1; i>=0; i--) {
@@ -58,5 +63,75 @@ public class ArrayEX {
         return newNumber;
     }
 
+    public int[] twoSum(int[] numbers, int target) {
+        int i=0,j=numbers.length-1;
+        int sum;
+        while (i<j){
+            sum=numbers[i]+numbers[j];
+            if (sum==target)
+                return new int[]{i+1,j+1};
+            else if (sum<target)
+                i++;
+            else
+                j--;
+        }
+        return null;
+//        int[] result=new int[2];
+//        for (int i=0;i<numbers.length-1;i++){
+//            for (int j=i+1;j<numbers.length;j++){
+//                if (numbers[i]+numbers[j]==target){
+//                    result[0]=i+1;
+//                    result[1]=j+1;
+//                }
+//            }
+//        }
+//        return result;
+    }
+
+    public void moveZeroes(int[] nums) {
+        int count=0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]!=0){
+                nums[count]=nums[i];
+                count++;
+            }
+        }
+
+        for (int i=count;i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+
+        int[] temp=new int[nums.length];
+        for (int i=0;i<temp.length;i++){
+            int index_new=(i+k)%nums.length;
+            temp[index_new]=nums[i];
+        }
+        for (int i=0;i<nums.length;i++){
+            nums[i]=temp[i];
+        }
+   }
+
+    public int removeElement(int[] nums, int val) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if (nums[i]==val)
+                count++;
+        }
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]==val){
+                for (int j=i+1;j<nums.length;j++){
+                    if (nums[j]!=val) {
+                        nums[i] = nums[j];
+                        nums[j] = val;
+                        break;
+                    }
+                }
+            }
+        }
+        return nums.length-count;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,25 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    private int n;
+    private int m;
+
     public static void main(String[] args) {
 
         /**
          * 输入长度不定的int数组
          */
-        Scanner in=new Scanner(System.in);
-        String[] s=null;
-        s=in.nextLine().split(",");
-        int[] nums=new int[s.length];
-        for(int i=0;i<s.length;i++){
-            nums[i]=Integer.parseInt(s[i]);
-        }
-        int target=in.nextInt();
+//        Scanner in=new Scanner(System.in);
+//        String[] s=null;
+//        char []s=in.nextLine().toCharArray();
+//        int[] nums=new int[s.length];
+//        for(int i=0;i<s.length;i++){
+//            nums[i]=Integer.parseInt(s[i]);
+//        }
+//        int target=in.nextInt();
+//
+//        Main main=new Main();
+//        main.numIslands();
 
         //System.out.println(searchinsertpos(nums,target));
 
@@ -27,7 +33,6 @@ public class Main {
      *map，collection的用法
      * map存储数字及其出现次数
      */
-
     public static int findShortestSubArray(int[] nums) {
         Map<Integer,Integer> left=new HashMap<>(),right=new HashMap<>(),count=new HashMap<>();
         for (int i=0;i<nums.length;i++){
@@ -46,5 +51,22 @@ public class Main {
         }
         return ans;
     }
+
+//    public int numIslands(char[][] grid) {
+//        int count=0;
+//        n=grid.length;
+//        m=grid[0].length;
+//        for (int i=0;i<n;i++){
+//            for (int j=0;j<m;j++){
+//
+//            }
+//        }
+//    }
+//    public void DFS(char[][] grid,int i,int j){
+//        if (i<0||j<0||i>=n||j>=m||grid[i][j]!='1')return;
+//        grid[i][j]='0';
+//
+//
+//    }
 
 }
